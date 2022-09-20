fcae219a1398b2cf4033d73ef81d2261397b8e5a
aldo <7622817+aldopareja@users.noreply.github.com>
2022-09-20 15:55:12 -0500

    finished the transformer posterior and now trying to learn the posterior over rotations and so on

--- flax_transformer_v2.py
+++ flax_transformer_v2.py
@@ -8,19 +8,22 @@ from jax.random import split, PRNGKey
 from flax import struct
 from flax import linen as nn
 
+
 @struct.dataclass
 class TransformerConfig:
-  """Global hyperparameters used to minimize obnoxious kwarg plumbing."""
-  num_heads: int = 4
-  num_enc_layers: int = 1
-  num_dec_layers: int = 1
-  dropout_rate: float = 0.1
-  deterministic: bool = False
-  d_model: int = 100
-  add_positional_encoding = False
-  max_len: int = 3000 #positional encoding
-  obs_emb_hidden_sizes: Sequence[int] = (200,)
-  num_mixtures: int = 2
+    """Global hyperparameters used to minimize obnoxious kwarg plumbing."""
+
+    num_heads: int = 4
+    num_enc_layers: int = 1
+    num_dec_layers: int = 1
+    dropout_rate: float = 0.1
+    deterministic: bool = False
+    d_model: int = 100
+    add_positional_encoding = False
+    max_len: int = 3000  # positional encoding
+    obs_emb_hidden_sizes: Sequence[int] = (200,)
+    num_mixtures: int = 2
+
 
 class ObsEmbed(nn.Module):
     config: TransformerConfig
@@ -30,279 +33,310 @@ class ObsEmbed(nn.Module):
         for feat in self.config.obs_emb_hidden_sizes:
             x = nn.Dense(feat)(x)
             x = nn.relu(x)
-        
+
         x = nn.Dense(self.config.d_model)(x)
-        
+
         return x
+
 
 class PositionalEncoder(nn.Module):
     config: TransformerConfig
-    #todo: shall we add dropout? there's documentation to read though.
-    
+    # todo: shall we add dropout? there's documentation to read though.
+
     @staticmethod
     def init_pe(d_model: int, max_length: int):
         positions = jnp.arange(max_length)[:, None]
-        div_term = jnp.exp(jnp.arange(0, d_model) * (-jnp.log(10000.0)/d_model))
-        
+        div_term = jnp.exp(jnp.arange(0, d_model) * (-jnp.log(10000.0) / d_model))
+
         temp = positions * div_term
         even_mask = positions % 2 == 0
-        
+
         pe = jnp.where(even_mask, jnp.sin(temp), jnp.cos(temp))
-        
-        return pe[None,:,:]
-    
-    
+
+        return pe[None, :, :]
+
     @nn.compact
     def __call__(self, x):
         cfg = self.config
-        pe = self.variable('consts', 'pe', PositionalEncoder.init_pe, cfg.d_model, cfg.max_len)
+        pe = self.variable(
+            "consts", "pe", PositionalEncoder.init_pe, cfg.d_model, cfg.max_len
+        )
         # batch_apply_pe = nn.vmap(lambda x, pe: x + pe[:x.shape[0]], in_axes=(0,None))
-        return x + pe.value[:,:x.shape[1]]
+        return x + pe.value[:, : x.shape[1]]
+
 
 class MlpBlock(nn.Module):
     """Transformer MLP / feed-forward block.
     Args:
       config: TransformerConfig dataclass containing hyperparameters.
     """
+
     config: TransformerConfig
 
     @nn.compact
     def __call__(self, inputs):
         """Applies Transformer MlpBlock module."""
         cfg = self.config
-        x = nn.Dense(
-            cfg.d_model * 2)(inputs)
+        x = nn.Dense(cfg.d_model * 2)(inputs)
         x = nn.relu(x)
-        x = nn.Dropout(rate=cfg.dropout_rate)(
-            x, deterministic=cfg.deterministic)
+        x = nn.Dropout(rate=cfg.dropout_rate)(x, deterministic=cfg.deterministic)
         output = nn.Dense(cfg.d_model)(x)
         output = nn.Dropout(rate=cfg.dropout_rate)(
-            output, deterministic=cfg.deterministic)
+            output, deterministic=cfg.deterministic
+        )
         return output
 
 
 class EncoderLayer(nn.Module):
-  """Transformer encoder layer.
-  Args:
-    config: TransformerConfig dataclass containing hyperparameters.
-  """
-  config: TransformerConfig
-
-  @nn.compact
-  def __call__(self,
-               inputs):
-    """Applies EncoderBlock module.
+    """Transformer encoder layer.
     Args:
-      inputs: input data for decoder
-    Returns:
-      output after transformer encoder block.
+      config: TransformerConfig dataclass containing hyperparameters.
     """
-    cfg = self.config
 
-    # Encoder block.
-    assert inputs.ndim == 3
-    x = nn.LayerNorm()(inputs)
-    x = nn.SelfAttention(
-        num_heads=cfg.num_heads,
-        use_bias=False, #should we use bias? I guess it doesn't matter
-        broadcast_dropout=False,
-        dropout_rate=cfg.dropout_rate,
-        deterministic=cfg.deterministic,
-        decode=False)(x)
-    x = nn.Dropout(rate=cfg.dropout_rate)(
-        x, deterministic=cfg.deterministic)
-    x = x + inputs
+    config: TransformerConfig
 
-    # MLP block.
-    z = nn.LayerNorm()(x)
-    z = MlpBlock(config=cfg)(z)
+    @nn.compact
+    def __call__(self, inputs):
+        """Applies EncoderBlock module.
+        Args:
+          inputs: input data for decoder
+        Returns:
+          output after transformer encoder block.
+        """
+        cfg = self.config
 
-    return x + z
+        # Encoder block.
+        assert inputs.ndim == 3
+        x = nn.LayerNorm()(inputs)
+        x = nn.SelfAttention(
+            num_heads=cfg.num_heads,
+            use_bias=False,  # should we use bias? I guess it doesn't matter
+            broadcast_dropout=False,
+            dropout_rate=cfg.dropout_rate,
+            deterministic=cfg.deterministic,
+            decode=False,
+        )(x)
+        x = nn.Dropout(rate=cfg.dropout_rate)(x, deterministic=cfg.deterministic)
+        x = x + inputs
+
+        # MLP block.
+        z = nn.LayerNorm()(x)
+        z = MlpBlock(config=cfg)(z)
+
+        return x + z
+
 
 class DecoderLayer(nn.Module):
-  """Transformer encoder-decoder layer.
-  Args:
-    config: TransformerConfig dataclass containing hyperparameters.
-  """
-  config: TransformerConfig
-
-  @nn.compact
-  def __call__(self, output_emb, encoded_input):
-    """Applies EncoderBlock module.
+    """Transformer encoder-decoder layer.
     Args:
-      inputs: input data for decoder
-    Returns:
-      output after transformer encoder block.
+      config: TransformerConfig dataclass containing hyperparameters.
     """
-     
-    cfg = self.config
 
-    # Decoder block.
-    assert encoded_input.ndim == 3 and output_emb.ndim == 3
-    x = nn.LayerNorm()(output_emb)
-    x = nn.SelfAttention(
-        num_heads=cfg.num_heads,
-        use_bias=False,
-        broadcast_dropout=False,
-        dropout_rate=cfg.dropout_rate,
-        deterministic=cfg.deterministic,
-        decode=False)(x)
-    x = nn.Dropout(rate=cfg.dropout_rate)(
-        x, deterministic=cfg.deterministic)
-    x = x + output_emb
+    config: TransformerConfig
 
-    z = nn.LayerNorm()(x)
+    @nn.compact
+    def __call__(self, output_emb, encoded_input):
+        """Applies EncoderBlock module.
+        Args:
+          inputs: input data for decoder
+        Returns:
+          output after transformer encoder block.
+        """
 
-    x = nn.MultiHeadDotProductAttention(num_heads=cfg.num_heads,
-        use_bias=False,
-        broadcast_dropout=False,
-        dropout_rate=cfg.dropout_rate,
-        deterministic=cfg.deterministic,
-        decode = False)(z, encoded_input)
+        cfg = self.config
 
-    x = x + z
+        # Decoder block.
+        assert encoded_input.ndim == 3 and output_emb.ndim == 3
+        x = nn.LayerNorm()(output_emb)
+        x = nn.SelfAttention(
+            num_heads=cfg.num_heads,
+            use_bias=False,
+            broadcast_dropout=False,
+            dropout_rate=cfg.dropout_rate,
+            deterministic=cfg.deterministic,
+            decode=False,
+        )(x)
+        x = nn.Dropout(rate=cfg.dropout_rate)(x, deterministic=cfg.deterministic)
+        x = x + output_emb
 
-    # MLP block.
-    z = nn.LayerNorm()(x)
+        z = nn.LayerNorm()(x)
 
-    z = MlpBlock(config=cfg)(z)
+        x = nn.MultiHeadDotProductAttention(
+            num_heads=cfg.num_heads,
+            use_bias=False,
+            broadcast_dropout=False,
+            dropout_rate=cfg.dropout_rate,
+            deterministic=cfg.deterministic,
+            decode=False,
+        )(z, encoded_input)
 
-    return x + z
+        x = x + z
+
+        # MLP block.
+        z = nn.LayerNorm()(x)
+
+        z = MlpBlock(config=cfg)(z)
+
+        return x + z
+
 
 class TransformerStack(nn.Module):
-  config: TransformerConfig
+    config: TransformerConfig
 
-  @nn.compact
-  def __call__(self, q):
-    cfg = self.config
-    x = ObsEmbed(cfg)(q)
-    enc_input = PositionalEncoder(cfg)(x) if cfg.add_positional_encoding else x
+    @nn.compact
+    def __call__(self, q):
+        cfg = self.config
+        x = ObsEmbed(cfg)(q)
+        enc_input = PositionalEncoder(cfg)(x) if cfg.add_positional_encoding else x
 
-    # for _ in range(cfg.num_enc_layers):
-    enc_input = nn.Sequential([EncoderLayer(cfg) for _ in range(cfg.num_enc_layers)])(enc_input)
+        # for _ in range(cfg.num_enc_layers):
+        enc_input = nn.Sequential(
+            [EncoderLayer(cfg) for _ in range(cfg.num_enc_layers)]
+        )(enc_input)
 
-    start = self.param('start', nn.initializers.uniform(),(1,1,cfg.d_model))
-    so_far_dec = start.repeat(q.shape[0],axis=0)
-    
+        start = self.param("start", nn.initializers.uniform(), (1, 1, cfg.d_model))
+        so_far_dec = start.repeat(q.shape[0], axis=0)
 
-    seq_decoded = []
-    for _ in range(cfg.num_mixtures):
-      dec = so_far_dec
-      # dec = decoder(dec, enc_input)
-      for _ in range(cfg.num_dec_layers):
-        dec = DecoderLayer(cfg)(dec,enc_input)
-      so_far_dec = jnp.concatenate([so_far_dec, jax.lax.stop_gradient(dec[:,-1:])],axis=1)
-      seq_decoded.append(dec[:,-1:,:])
-    seq_decoded = jnp.concatenate(seq_decoded, axis=1)
+        seq_decoded = []
+        for _ in range(cfg.num_mixtures):
+            dec = so_far_dec
+            # dec = decoder(dec, enc_input)
+            for _ in range(cfg.num_dec_layers):
+                dec = DecoderLayer(cfg)(dec, enc_input)
+            so_far_dec = jnp.concatenate(
+                [so_far_dec, jax.lax.stop_gradient(dec[:, -1:])], axis=1
+            )
+            seq_decoded.append(dec[:, -1:, :])
+        seq_decoded = jnp.concatenate(seq_decoded, axis=1)
 
-    return seq_decoded
+        return seq_decoded
+
 
 @struct.dataclass
 class GaussianMixturePosteriorConfig:
-  num_latents: int = 2
-  emb_size: int = 100
-  covariance_eps: float = 1e-6
+    num_latents: int = 2
+    emb_size: int = 100
+    covariance_eps: float = 1e-6
+
 
 class TransformerGaussianMixturePosterior(nn.Module):
-  config: GaussianMixturePosteriorConfig
+    config: GaussianMixturePosteriorConfig
 
-  @nn.compact
-  def __call__(self, seq_decoded):
-    '''takes the output of a transformer decoder and outputs the parameters of a multivariate gaussian mixture
-    '''
-    cfg = self.config
-    num_means = cfg.num_latents
-    num_covariance_terms = int(num_means * (num_means + 1) / 2)
-    num_mixture_params = 1 + num_means + num_covariance_terms
+    @nn.compact
+    def __call__(self, seq_decoded):
+        """takes the output of a transformer decoder and outputs the parameters of a multivariate gaussian mixture"""
+        cfg = self.config
+        num_means = cfg.num_latents
+        num_covariance_terms = int(num_means * (num_means + 1) / 2)
+        num_mixture_params = 1 + num_means + num_covariance_terms
 
-    dist_params = nn.Sequential([nn.Dense(cfg.emb_size),
-                                nn.relu,
-                                nn.Dense(num_mixture_params)])(seq_decoded)
+        dist_params = nn.Sequential(
+            [nn.Dense(cfg.emb_size), nn.relu, nn.Dense(num_mixture_params)]
+        )(seq_decoded)
 
-    mix_p = jnp.exp(dist_params[:,:,0])
-    means = dist_params[:,:, 1: 1+num_means]
-    covariance_terms = dist_params[:,:,-num_covariance_terms:]
-    covariance_matrices = self.get_cov_matrices_from_vectors(covariance_terms, num_means, cfg.covariance_eps)
+        mix_p = jnp.exp(dist_params[:, :, 0])
+        means = dist_params[:, :, 1 : 1 + num_means]
+        covariance_terms = dist_params[:, :, -num_covariance_terms:]
+        covariance_matrices = self.get_cov_matrices_from_vectors(
+            covariance_terms, num_means, cfg.covariance_eps
+        )
 
-    return dict(mix_p=mix_p/mix_p.sum(axis=1,keepdims=True), means=means, covariance_matrices=covariance_matrices)
+        return dict(
+            mix_p=mix_p / mix_p.sum(axis=1, keepdims=True),
+            means=means,
+            covariance_matrices=covariance_matrices,
+        )
 
-  @staticmethod
-  def get_cov_matrices_from_vectors(covariance_terms, num_means, eps):
-    x = covariance_terms
-    output_shape = (*x.shape[:-1], num_means, num_means)
-    x = jnp.concatenate([x, x[:,:,num_means:][:,:,::-1]], axis=-1)
-    x = x.reshape(output_shape)
-    x = jnp.triu(x)
+    @staticmethod
+    def get_cov_matrices_from_vectors(covariance_terms, num_means, eps):
+        x = covariance_terms
+        output_shape = (*x.shape[:-1], num_means, num_means)
+        x = jnp.concatenate([x, x[:, :, num_means:][:, :, ::-1]], axis=-1)
+        x = x.reshape(output_shape)
+        x = jnp.triu(x)
 
-    eps = jnp.eye(num_means)[None,None] * eps
-    cov_matrices = jnp.matmul(x, x.swapaxes(-2,-1)) + eps
-    return cov_matrices
+        eps = jnp.eye(num_means)[None, None] * eps
+        cov_matrices = jnp.matmul(x, x.swapaxes(-2, -1)) + eps
+        return cov_matrices
+
 
 def gaussian_mixture_logpdf(latents, dist_params):
-  ''' 
-  Parameters:
-  --------------
-  latents: jax.array with shape Batch x 1 x num_latents
-    the single dimension is needed so that the same sample gets broadcasted through all mixtures
-  dist_params: Dict(
-          mix_p: jax.array with shape Batch x num_mixtures encoding the probability of each mixture
-          means: jax.array with shape Batch x num_mixtures x num_means with the multivariate mean of each mixture
-          cov_matrices: jax.array with shape Batch x num_mixtures x num_means x num_means 
-          )         
-  '''
-  mix_p, means, covs = dist_params['mix_p'], dist_params['means'], dist_params['covariance_matrices']
-  
-  normals_log_prob = jax.scipy.stats.multivariate_normal.logpdf(latents, means, covs)
-  category_log_prob = jax.lax.log(mix_p)
-  
-  return jax.nn.logsumexp(normals_log_prob + category_log_prob, axis=1)
+    """
+    Parameters:
+    --------------
+    latents: jax.array with shape Batch x 1 x num_latents
+      the single dimension is needed so that the same sample gets broadcasted through all mixtures
+    dist_params: Dict(
+            mix_p: jax.array with shape Batch x num_mixtures encoding the probability of each mixture
+            means: jax.array with shape Batch x num_mixtures x num_means with the multivariate mean of each mixture
+            cov_matrices: jax.array with shape Batch x num_mixtures x num_means x num_means
+            )
+    """
+    mix_p, means, covs = (
+        dist_params["mix_p"],
+        dist_params["means"],
+        dist_params["covariance_matrices"],
+    )
+
+    normals_log_prob = jax.scipy.stats.multivariate_normal.logpdf(latents, means, covs)
+    category_log_prob = jax.lax.log(mix_p)
+
+    return jax.nn.logsumexp(normals_log_prob + category_log_prob, axis=1)
+
 
 @struct.dataclass
 class IndependentGaussianMixtureConfig:
-  #sequence with the number of variables on each group (each group modeled with a gaussian mixture)
-  group_variables: Sequence[int] = (3, 4)
-  #number of mixtures used on each group
-  num_mixtures_per_group: Sequence[int] = (2,8)
+    # sequence with the number of variables on each group (each group modeled with a gaussian mixture)
+    group_variables: Sequence[int] = (3, 4)
+    # number of mixtures used on each group
+    num_mixtures_per_group: Sequence[int] = (2, 8)
+
 
 class IndependentGaussianMixtures(nn.Module):
-  config: IndependentGaussianMixtureConfig
-  mixtures_cfg: GaussianMixturePosteriorConfig
-  transformer_cfg: TransformerConfig
+    config: IndependentGaussianMixtureConfig
+    mixtures_cfg: GaussianMixturePosteriorConfig
+    transformer_cfg: TransformerConfig
 
-  @nn.compact
-  def __call__(self, q):
-    '''
-    Parameters: 
-      q: array with shape B x num_obs x seq_length
-        contains a batch of sequences of observable variables. In the case of a point cloud
-        this contains Batch size x 3 x num_points (since each point is x,y,z).
-    returns:
-      list_dist_params: list of dicts, len(list_dist_params) = len(group_varibles)
-        list of dicts where each dict contains batched parameters of a multivariate gaussian mixture.
-        see :meth:`gaussian_mixture_logpdf`
-    '''
-    cfg = self.config
-    transformer_cfg = self.transformer_cfg.replace(num_mixtures=sum(cfg.num_mixtures_per_group))
-    mixtures_cfg = self.mixtures_cfg
+    @nn.compact
+    def __call__(self, q):
+        """
+        Parameters:
+          q: array with shape B x num_obs x seq_length
+            contains a batch of sequences of observable variables. In the case of a point cloud
+            this contains Batch size x 3 x num_points (since each point is x,y,z).
+        returns:
+          list_dist_params: list of dicts, len(list_dist_params) = len(group_varibles)
+            list of dicts where each dict contains batched parameters of a multivariate gaussian mixture.
+            see :meth:`gaussian_mixture_logpdf`
+        """
+        cfg = self.config
+        transformer_cfg = self.transformer_cfg.replace(
+            num_mixtures=sum(cfg.num_mixtures_per_group)
+        )
+        mixtures_cfg = self.mixtures_cfg
 
-    seq_decoded = TransformerStack(transformer_cfg)(q)
-    
-    v = jnp.cumsum(jnp.array((0, *cfg.num_mixtures_per_group)))
-    seq_dec_list = [seq_decoded[:,v[i]:v[i+1]] for i in range(len(v)-1)]
-    dist_params_list = []
-    for seq_dec,n_vars in zip(seq_dec_list, cfg.group_variables):
-      d_pars = TransformerGaussianMixturePosterior(mixtures_cfg.replace(num_latents=n_vars))(seq_dec)
-      dist_params_list.append(d_pars)
-    
-    return dist_params_list
+        seq_decoded = TransformerStack(transformer_cfg)(q)
+
+        v = jnp.cumsum(jnp.array((0, *cfg.num_mixtures_per_group)))
+        seq_dec_list = [seq_decoded[:, v[i] : v[i + 1]] for i in range(len(v) - 1)]
+        
+        dist_params_list = []
+        for seq_dec, n_vars in zip(seq_dec_list, cfg.group_variables):
+            d_pars = TransformerGaussianMixturePosterior(
+                mixtures_cfg.replace(num_latents=n_vars)
+            )(seq_dec)
+            dist_params_list.append(d_pars)
+
+        return dist_params_list
+
 
 if __name__ == "__main__":
-  m = IndependentGaussianMixtures(IndependentGaussianMixtureConfig(), GaussianMixturePosteriorConfig(), TransformerConfig())
-  key, *subkeys = split(PRNGKey(11234), 4)
-  init_rngs = {'params': subkeys[0], 'dropout':subkeys[1]}
-  variables = m.init(init_rngs, jnp.ones((batch_size, obs_lenght, 1)))
-  m.init(init_rngs,jnp.ones((100,6,999)))
-    
-
-
-
+    m = IndependentGaussianMixtures(
+        IndependentGaussianMixtureConfig(),
+        GaussianMixturePosteriorConfig(),
+        TransformerConfig(),
+    )
+    key, *subkeys = split(PRNGKey(11234), 4)
+    init_rngs = {"params": subkeys[0], "dropout": subkeys[1]}
+    variables = m.init(init_rngs, jnp.ones((batch_size, obs_lenght, 1)))
+    m.init(init_rngs, jnp.ones((100, 6, 999)))
---
+++ learn_brax.py
@@ -0,0 +1,260 @@
+from copy import deepcopy
+
+import numpy as onp
+
+import jax
+from jax.random import split, PRNGKey, uniform
+from jax import numpy as jnp
+
+import optax
+
+import brax
+from brax.io.image import _scene
+from brax.math import relative_quat
+
+from pytinyrenderer import TinyRenderCamera as Camera
+from pytinyrenderer import TinyRenderLight as Light
+from google.protobuf import text_format
+
+from istar_map_tqdm_patch import array_apply
+
+from flax_transformer_v2 import (
+    IndependentGaussianMixtures,
+    IndependentGaussianMixtureConfig,
+    GaussianMixturePosteriorConfig,
+    TransformerGaussianMixturePosterior,
+    TransformerConfig,
+    TransformerStack,
+    gaussian_mixture_logpdf,
+)
+
+_DEFAULT_CAMERA = {
+    "viewWidth": 640,
+    "viewHeight": 480,
+    "position": [1.5, -3.0, 1.625],
+    "target": [0.0, 0.0, 0],
+    "up": [0, 0, 1],
+    "hfov": 58.0,
+    "vfov": 43.5,
+}
+_DEFAULT_LIGHT = {
+    "direction": [0.57735, -0.57735, 0.57735],
+    "ambient": 0.8,
+    "diffuse": 0.8,
+    "specular": 0.6,
+    "shadowmap_center": [0.0, 0.0, 0],
+}
+_DEFAULT_CONFIG = """bodies {
+  name: "box"
+  colliders {
+    box {
+      halfsize {
+        x: 0.75
+        y: 0.25
+        z: 0.125
+      }
+    }
+  }
+  inertia {
+    x: 1.0
+    y: 1.0
+    z: 1.0
+  }
+  mass: 1.0
+}
+dt: 0.05000000074505806
+substeps: 20
+dynamics_mode: "pbd"
+"""
+default_env = text_format.Parse(_DEFAULT_CONFIG, brax.Config())
+
+
+def get_default_camera(width, height):
+    cam = deepcopy(_DEFAULT_CAMERA)
+    cam["vfov"] = cam["hfov"] * height / width
+    cam["viewWidth"] = width * 1
+    cam["viewHeight"] = height * 1
+    cam["position"] = [c * 2 for c in cam["position"]]
+    return Camera(**cam)
+
+
+def get_default_light():
+    return Light(**_DEFAULT_LIGHT)
+
+
+def render_depth_array(env_cfg, qp_dict, height=320, width=320):
+    sys = brax.System(env_cfg)
+    my_qp = brax.QP(**qp_dict)
+    scene, instances = _scene(sys, my_qp)
+    camera = get_default_camera(width=width, height=height)
+    x = scene.get_camera_image(instances, get_default_light(), camera)
+    x = onp.array(x.depthbuffer)
+    x = onp.reshape(x, (camera.view_height, camera.view_width))
+    return x
+
+
+def get_point_cloud_from_depth(depth_map: onp.ndarray, sample_size):
+    """gets a point cloud of `sample_size` numbers from `depth_map`
+    It first ignores the far plane, then computes x,y,z as the x,y pixel coordinates and
+    sets z as the depth value in `depth_array`.
+
+    Parameters:
+    -------------
+        depth_map: onp.ndarray
+            Height x Width array with the depth values of each pixel
+
+    Returns:
+    -------------
+        point_cloud: onp.ndarray
+            `sample_size` x 3 array with each row having the x,y,z coordinates of each
+            point
+    """
+    non_far_mask = depth_map > -1e4
+    x, y = non_far_mask.nonzero()
+    z = depth_map[x, y]
+    point_clouds = onp.stack([x, y, z], axis=1)
+    idx = onp.random.randint(0, point_clouds.shape[0], sample_size)
+    return point_clouds[idx]
+
+
+def render_point_cloud(env_cfg, qp_dict, height=320, width=320, sample_size=1000):
+    depth = render_depth_array(env_cfg, qp_dict, height, width)
+    point_cloud = get_point_cloud_from_depth(depth, sample_size)
+    return point_cloud
+
+
+def sample_box_rotation(rng: PRNGKey, angle=None, img=None):
+    # sample imaginary part of quaternion (axis of rotation)
+    key, *subkeys = split(rng, 4)
+    img = (
+        uniform(subkeys[0], minval=-1.0, maxval=1.0, shape=(3,)) + 1e-6
+        if img is None
+        else img
+    )
+    img = img / jnp.linalg.norm(img)
+
+    # sample angle (how much to rotate)
+    angle = (
+        uniform(subkeys[1], minval=-2 * jnp.pi, maxval=2 * jnp.pi)
+        if angle is None
+        else angle
+    )
+    w = jnp.cos(angle / 2)[None]
+    img = jnp.sin(angle / 2) * img
+    rot = jnp.concatenate([w, img])
+    return rot
+
+
+def sample_qp(key: PRNGKey):
+    key, subkey = split(key)
+    # position of each body in 3d (z is up, right-hand coordinates)
+    pos = uniform(key, minval=-2.0, maxval=2.0, shape=(3,))[None]
+    # velocity of each body in 3d
+    vel = jnp.array([[0.0, 0.0, 0.0]])
+    # rotation about center of body, as a quaternion (w, x, y, z)
+    rot = sample_box_rotation(subkey)[None]
+    # angular velocity about center of body in 3d
+    ang = jnp.array([[0.0, 0.0, 0.0]])
+    return dict(pos=pos, rot=rot, vel=vel, ang=ang)
+
+
+vj_sample_qp = jax.jit(jax.vmap(sample_qp))
+
+
+@jax.jit
+def compute_diffs(batch_qp):
+    init = jax.tree_map(lambda x: x[0 : len(x) : 2], batch_qp)
+    final = jax.tree_map(lambda x: x[1 : len(x) : 2], batch_qp)
+    pos_diff = (final["pos"] - init["pos"])[:, 0]  # remove unnecessary object dimension
+    rot_diff = jax.vmap(relative_quat, in_axes=(0, 0))(
+        init["rot"][:, 0], final["rot"][:, 0]
+    )
+    return dict(pos_diff=pos_diff[:,None], rot_diff=rot_diff[:,None])
+
+
+def generate_data_batch(
+    key: PRNGKey, batch_size, height=320, width=320, num_points=999
+):
+    batch_qp = vj_sample_qp(split(key, batch_size * 2))
+    diffs = compute_diffs(batch_qp)
+    batch_qp, diffs = jax.tree_map(lambda x: onp.array(x), [batch_qp, diffs])
+    # diffs = jax.tree_map(lambda x: onp.array(x), diffs)
+    args = [
+        (default_env, jax.tree_map(lambda x: x[i], batch_qp), height, width, num_points)
+        for i in range(batch_size * 2)
+    ]
+    point_clouds = array_apply(render_point_cloud, args, True, chunksize=1000)
+    point_clouds = onp.stack(point_clouds)
+
+    # concatenate in pairs since we want to predict differences
+    point_clouds = onp.concatenate(
+        [point_clouds[0 : batch_size * 2 : 2], point_clouds[1 : batch_size * 2 : 2]],
+        axis=-1,
+    )
+    return diffs, point_clouds
+
+
+def update_step(apply_fn, p_clouds, latents_list, opt_state, params, state, dropout_key, tx):
+    def loss(params):
+        d_params_list = apply_fn(
+            {"params": params, **state}, p_clouds, rngs={"dropout": dropout_key}
+        )
+        l = sum(
+            [
+                -gaussian_mixture_logpdf(l, d).mean()
+                for l, d in zip(latents_list, d_params_list)
+            ]
+        )
+        return l
+
+    l, grads = jax.value_and_grad(loss, has_aux=False)(params)
+    updates, opt_state = tx.update(grads, opt_state)
+    params = optax.apply_updates(params, updates)
+    return opt_state, params, l
+
+
+if __name__ == "__main__":
+    key, *sks = split(PRNGKey(12345), 10)
+    generation_size = 10000
+    batch_size = 100
+    obs_length = 999
+    num_epochs = 999999
+
+    m = IndependentGaussianMixtures(
+        IndependentGaussianMixtureConfig(
+            group_variables=(3, 4), num_mixtures_per_group=(2, 8)
+        ),
+        GaussianMixturePosteriorConfig(),
+        TransformerConfig(),
+    )
+
+    variables = m.init(
+        {"params": sks[0], "dropout": sks[1]}, jnp.ones((batch_size, obs_length, 6))
+    )
+    state, params = variables.pop('params')
+    del variables
+
+    tx = optax.adam(learning_rate=0.001)
+    opt_state = tx.init(params)
+
+    for e in range(num_epochs):
+
+        key, subkey = split(key)
+        diffs, p_clouds = generate_data_batch(subkey, generation_size, num_points=obs_length)
+        for i in range(generation_size//batch_size - 1):
+            key, subkey = split(key)
+            opt_state, params, l = update_step(
+                m.apply,
+                p_clouds = jnp.array(p_clouds[i*batch_size: (i+1)*batch_size]),
+                latents_list = [jnp.array(diffs['pos_diff'][i*batch_size: (i+1)*batch_size]), 
+                                jnp.array(diffs['rot_diff'][i*batch_size: (i+1)*batch_size])],
+                opt_state=opt_state,
+                params = params,
+                state = state,
+                dropout_key=subkey,
+                tx=tx,
+            )
+            if jnp.isnan(l) or l == 0:
+                print('failed',e,i,l)
+                finish = True
+                break
